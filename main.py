4e3c2c10b8c7a4b3650080499a53c5f2f242194b
konst1970 <konst1970@gmail.com>
2021-03-06 01:42:31 +0200

    Update main.py
    form node list in add_component()

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ class CurrentSource(Component):
         self.nodes = [i,j]
         self.args = current
 
-def print_circuit(name):
+def run_test_circuit(name):
     R1 = Resistor(0, 1, 100) # 100 Ohm
     V1 = VoltageSource(1, 2, 1) # 1 Volt
     I1 = CurrentSource(0, 1, 1) # 1 Amper
@@ -31,9 +31,36 @@ def print_circuit(name):
     V1.print()
     I1.print()
 
+    test_circuit = Circuit()
+
+    test_circuit.add_component(R1)
+    test_circuit.add_component(V1)
+    test_circuit.add_component(I1)
+
+    test_circuit.print()
+
+    test_circuit.calc_all_nodes()
+
+class Circuit():
+    def __init__(self):
+        self.nodes = []
+        self.components = []
+
+    def print(self):
+        print(self.nodes)
+        print(self.components)
+
+    def add_component(self, component):
+        self.components = self.components + [component]
+        self.nodes = list(set(self.nodes) | set(component.nodes))
+
+    def calc_all_nodes(self):
+        for component in self.components:
+            print (component.nodes)
+            print (component.args)
 
 if __name__ == '__main__':
     print("DNIPRO v.0.0.1 (c) 2020, all rights reserved")
-    print_circuit("Test Circuit")
+    run_test_circuit("Test Circuit")
 
 
