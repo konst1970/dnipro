197acb94f3ace048c39f0e7eca0225d241f88f31
konst1970 <konst1970@gmail.com>
2021-03-06 03:34:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import numpy
 
 class Component():
     def __init__(self, nodes, args):
         self.nodes = nodes
         self.args = args
+
+    def add(self, circuit): # must be overloaded in each component
+        return 0
 
     def print(self):
         print(self.nodes)
@@ -12,6 +16,31 @@ class Resistor(Component):
     def __init__(self, i, j, resistance):
         self.nodes = [i,j]
         self.args = resistance
+
+    def add(self, circuit):
+        print ("R(", self.nodes[0], ",", self.nodes[1], ")=", self.args)
+        # find my index in component list
+        ind = circuit.components.index (self)
+        print ("Index=", ind)
+
+        i = self.nodes[0]
+        j = self.nodes[1]
+
+        if (i > 0):
+          circuit.A[ind, i-1] = -1
+          circuit.A[len(circuit.components)+i-1,len(circuit.nodes)+ind-1] = -1
+
+        if (j > 0):
+          circuit.A[ind, j-1] = 1
+          circuit.A[len(circuit.components)+j-1,len(circuit.nodes)+ind-1] = 1
+
+        circuit.A[ind, len(circuit.components)+len(circuit.nodes)-1+ind] = -1
+        circuit.A[len(circuit.components)+len(circuit.nodes)-1+ind,
+                  len(circuit.components)+len(circuit.nodes)-1+ind] = 1
+        circuit.A[len(circuit.components)+len(circuit.nodes)-1+ind,
+                 len(circuit.nodes)-1+ind] = -self.args
+
+
 
 class VoltageSource(Component):
     def __init__(self, i, j, voltage):
@@ -23,44 +52,91 @@ class CurrentSource(Component):
         self.nodes = [i,j]
         self.args = current
 
-def run_test_circuit(name):
-    R1 = Resistor(0, 1, 100) # 100 Ohm
-    V1 = VoltageSource(1, 2, 1) # 1 Volt
-    I1 = CurrentSource(0, 1, 1) # 1 Amper
-    R1.print()
-    V1.print()
-    I1.print()
+    def add(self, circuit):
+        print ("I(", self.nodes[0], ",", self.nodes[1], ")=", self.args)
+        # find my index in component list
+        ind = circuit.components.index (self)
+        print ("Index=", ind)
 
-    test_circuit = Circuit()
+        i = self.nodes[0]
+        j = self.nodes[1]
 
-    test_circuit.add_component(R1)
-    test_circuit.add_component(V1)
-    test_circuit.add_component(I1)
+        if (i > 0):
+          circuit.A[ind, i-1] = -1
+          circuit.A[len(circuit.components)+i-1,len(circuit.nodes)+ind-1] = -1
 
-    test_circuit.print()
+        if (j > 0):
+          circuit.A[ind, j-1] = 1
+          circuit.A[len(circuit.components)+j-1,len(circuit.nodes)+ind-1] = 1
 
-    test_circuit.calc_all_nodes()
+        circuit.A[ind, len(circuit.components)+len(circuit.nodes)-1+ind] = -1
+        circuit.A[len(circuit.components)+len(circuit.nodes)-1+ind,
+                 len(circuit.nodes)-1+ind] = -1
+        circuit.b[len(circuit.components)+len(circuit.nodes)-1+ind] = self.args
+
 
 class Circuit():
     def __init__(self):
         self.nodes = []
         self.components = []
+        self.A = numpy.zeros(shape=(1,1))
+        self.b = numpy.zeros(shape=(1,1))
+        self.x = numpy.zeros(shape=(1,1))
+        self.size = 1
 
     def print(self):
         print(self.nodes)
         print(self.components)
+        print(self.size)
+        print(self.A)
+        print(self.b)
+        print(self.x)
 
     def add_component(self, component):
         self.components = self.components + [component]
-        self.nodes = list(set(self.nodes) | set(component.nodes))
+        self.nodes = list(set(self.nodes) | set(component.nodes)) # add new nodes to list
 
-    def calc_all_nodes(self):
+    def calc_all_nodes_OTM(self):
+        self.size = len(self.nodes)-1 + 2*len(self.components)
+        self.A = numpy.zeros(shape=(self.size,self.size))
+        self.b = numpy.zeros(shape=(self.size,1))
+        self.x = numpy.zeros(shape=(self.size,1))
+
         for component in self.components:
-            print (component.nodes)
-            print (component.args)
+            component.add(self)
+
+    def solve(self):
+        self.calc_all_nodes_OTM()
+        self.x = numpy.linalg.solve(self.A, self.b)
+
+def run_test_circuit(name):
+    R1 = Resistor(0, 1, 2) # 2 Ohm
+    R2 = Resistor(1, 2, 1) # 1 Ohm
+    R3 = Resistor(0, 2, 1) # 1 Ohm
+    I1 = CurrentSource(0, 1, 1) # 1 Amper
+
+    # V1 = VoltageSource(0, 2, 1) # 1 Volt
+
+    # R1.print()
+    # R2.print()
+    # R3.print()
+    # V1.print()
+    # I1.print()
+
+    test_circuit = Circuit()
+
+    test_circuit.add_component(R1)
+    test_circuit.add_component(R2)
+    test_circuit.add_component(R3)
+    test_circuit.add_component(I1)
+
+    # test_circuit.add_component(V1)
+
+    test_circuit.solve()
+
+    test_circuit.print()
 
 if __name__ == '__main__':
-    print("DNIPRO v.0.0.1 (c) 2020, all rights reserved")
+    print("DNIPRO v.0.0.1 (c) 2021, all rights reserved")
     run_test_circuit("Test Circuit")
-
 
